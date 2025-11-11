38d3c499131a88371fdf46d8a249833bed5cf03f
VivianAr2409 <vivianar2409@gmail.com>
2025-11-10 20:41:21 -0800

    Agregar información del proyecto y versión - VivianAr2409

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+Sistema Escolar - Proyecto independiente
+Desarrollado por VivianAr2409
+
+Este sistema permite la gestión integral de datos escolares
+con herramientas de análisis estadístico y visualización.
+"""
+
+__version__ = "1.0.0"
+__author__ = "VivianAr2409"
+__email__ = "vivianar2409@gmail.com"
+__project__ = "Sistema Escolar"
